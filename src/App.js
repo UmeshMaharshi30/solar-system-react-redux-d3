1b367efaf8469efed568c2b3b018022d2f722ad6
UmeshMaharshi30 <umeshmaharshi@gmail.com>
2019-10-16 11:55:41 -0500

    template commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome !</h1>
+      <p>This is a react-redux template</p>
+    </div>
+  );
+}
+
+export default App;
