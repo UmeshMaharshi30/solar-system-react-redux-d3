df5c24b09fa3183d83b811ee62bdf9ef27b78daf
UmeshMaharshi30 <umeshmaharshi@gmail.com>
2019-10-18 01:36:48 -0500

    with ellipse trajectory

--- src/components/planet/planet.js
+++ src/components/planet/planet.js
@@ -23,28 +23,29 @@ class Planet extends Component {
 
     componentDidUpdate() {       
         let el = d3.select(this.planetRef.current);
-        var radius = this.props.config.distance * Math.SQRT2;
+        var x_radius = this.props.config.distance * (1.1);
+        var y_radius = this.props.config.distance * (0.8);
         let plant_ref = this;
         var arc = d3.arc()
-                    .innerRadius(radius)
-                    .outerRadius(radius + 0.5)
+                    .innerRadius(x_radius)
+                    .outerRadius(x_radius + 0.5)
                     .startAngle(0)  
                     .endAngle(2*Math.PI);
                     
         var trajectory = el.select('path');
             trajectory.attr("d", arc());
         var planet_body = el.select('circle');
-            planet_body.attr("cx", radius*Math.cos(Math.PI * 2 *Math.random())).attr("cy", radius*Math.sin(Math.PI * 2 *Math.random())).attr("angle", 0);
+            planet_body.attr("cx", x_radius*Math.cos(Math.PI * 2 *Math.random())).attr("cy", y_radius*Math.sin(Math.PI * 2 *Math.random())).attr("angle", 0);
         var direction = this.props.config.rotation_direction === true ? -1 : 1;
             planet_body.transition().duration(this.props.config.revolution_time).ease(d3.easeLinear)
                         .attrTween("cx", function() {
                             return function(t) {
-                                return radius*Math.cos(direction* Math.PI * 2 *t);
+                                return x_radius*Math.cos(direction* Math.PI * 2 *t);
                             };
                         })
                         .attrTween("cy", function() {
                             return function(t) {
-                                return radius*Math.sin(direction * Math.PI * 2 *t);
+                                return y_radius*Math.sin(direction * Math.PI * 2 *t);
                             };
                         }).on("end", function() {
                             plant_ref.setState({...this.state});
@@ -54,12 +55,15 @@ class Planet extends Component {
     render() {
         var planet_properties = this.state.config.config;
         var distance_from_sun = planet_properties.distance;
+        var rx = (1.1)*distance_from_sun;
+        var ry = (0.8)*distance_from_sun;
         var planet_size = planet_properties.size;
         var revolution_time = planet_properties.revolution_time;
         var rotation_time = planet_properties.rotation_time;
         var color = planet_properties.color;
+        var x_translate = 20 * planet_properties.revolution_direction;
         var radius = this.props.config.distance * Math.SQRT2;
-        return <g ref={this.planetRef} transform="translate(400,300)"><path fill={color} style={{"opacity" : 0.3}}></path><circle cx={radius*Math.cos(Math.PI * 2 *Math.random())} r={planet_size} cy={radius*Math.sin(Math.PI * 2 *Math.random())} fill={color} ></circle>
+        return <g ref={this.planetRef} transform={"translate(" + (400 + x_translate) + ",300)"}><ellipse rx={rx} ry={ry} fill="none" strokeWidth="2" style={{"opacity" : 0.3, "stroke" : color}}></ellipse><circle cx={radius*Math.cos(Math.PI * 2 *Math.random())} r={planet_size} cy={radius*Math.sin(Math.PI * 2 *Math.random())} fill={color} ></circle>
         </g>
         
     }
--- src/reducers/planet.config.js
+++ src/reducers/planet.config.js
@@ -16,24 +16,20 @@ const planetsReducer = (state = null, action) => {
     switch(action.type) {
         default:
             var total_planets = [];            
-            var planets = [{"name" : "Mercury", "size" : planet_size_scale(1.5), "color" : "gray", "distance" : 25, "revolution_time" : 1000, "rotation_time" :2000},
-            {"name" : "Venus", "size" : planet_size_scale(1.85), "color" : "LightYellow", "distance" : 60, "revolution_time" : 2000, "rotation_time" :2000},
-            {"name" : "Earth", "size" : 4, "color" : "blue", "distance" : 100, "revolution_time" : 4500, "rotation_time" :2000},
-            {"name" : "Mars", "size" : 3.75, "color" : "#654321", "distance" : 200, "revolution_time" : 6500, "rotation_time" :2000},
-            {"name" : "Jupiter", "size" : 3.75, "color" : "Orange", "distance" : 200, "revolution_time" : 6500, "rotation_time" :2000},
-            {"name" : "Saturn", "size" : 3.75, "color" : "PaleGoldenRod", "distance" : 200, "revolution_time" : 6500, "rotation_time" :2000},
-            {"name" : "Uranus", "size" : 3.75, "color" : "SlateBlue ", "distance" : 200, "revolution_time" : 6500, "rotation_time" :2000},
-            {"name" : "Neptune", "size" : 3.75, "color" : "#6081FF", "distance" : 200, "revolution_time" : 6500, "rotation_time" :2000},
-            {"name" : "Pluto", "size" : 3.75, "color" : "RosyBrown", "distance" : 200, "revolution_time" : 6500, "rotation_time" :2000}]
+            var planets = [{"name" : "Mercury","color" : "gray", "rotation_time" :2000},
+            {"name" : "Venus", "color" : "LightYellow", "rotation_time" :2000},
+            {"name" : "Earth",  "color" : "blue", "rotation_time" :2000},
+            {"name" : "Mars", "color" : "#654321","rotation_time" :2000},
+            {"name" : "Jupiter", "color" : "Orange", "rotation_time" :2000},
+            {"name" : "Saturn", "color" : "PaleGoldenRod", "rotation_time" :2000},
+            {"name" : "Uranus", "color" : "SlateBlue ", "rotation_time" :2000},
+            {"name" : "Neptune", "color" : "#6081FF", "rotation_time" :2000},
+            {"name" : "Pluto", "color" : "RosyBrown", "rotation_time" :2000}]
             for(let i = 0; i < 9; i++) {
                 planets[i].size = planet_size_scale(solar_system_config.diameter_ratio[i]);
                 planets[i].distance = planet_distance_scale(solar_system_config.distance_from_sun[i]);
                 planets[i].revolution_time = planet_revolution_scale(solar_system_config.revolution_period[i]);
-                /*
-                planets[i].size = planet_size_scale(solar_system_config.diameter_ratio[i]);
-                planets[i].distance = planet_distance_scale(solar_system_config.distance_from_sun[i]);
-                planets[i].revolution_time = planet_size_scale(solar_system_config.revolution_period[i]);
-                */
+                planets[i].revolution_direction = Math.random() > 0.5 ? 1 : -1;
             }
             return planets;
     }
